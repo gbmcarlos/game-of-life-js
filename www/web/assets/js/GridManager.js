58b16e4eb81688973caa41f0130382ebd3082a47
Carlos Garcia-Berro Molero <gbmcarlos@gmail.com>
2017-11-06 16:26:05 +0400

    svg instead of canvas, Grid manager can draw a whole grid and come up with a random one

--- www/web/assets/js/GridManager.js
+++ www/web/assets/js/GridManager.js
@@ -9,33 +9,111 @@
 
         config: {
             grid: {
-                width: '600',
-                height: '600',
-                containerSelector: '#grid-container',
-                id: 'grid'
+                width: 600,
+                height: 600,
+                selector: '#grid',
+                numberCellsX: 38,
+                numberCellsY: 38
             }
         },
 
         init: function() {
 
-            this.canvasElement = this.createCanvas(this.config.grid);
-
-
+            this.canvas = this.setupCanvas(this.config);
 
         },
 
-        createCanvas: function(gridConfig) {
+        getRandomGrid: function() {
 
-            var canvas = document.createElement('canvas');
-            canvas.id = gridConfig.id;
-            canvas.width = gridConfig.width;
-            canvas.height = gridConfig.height;
-            canvas.style.border = '1px solid';
+            var grid = [];
 
-            var container = this.$(gridConfig.containerSelector);
-            container.empty();
-            container.append(canvas);
+            for (var i = 0; i < this.config.grid.numberCellsX; i++) { // for each line
 
+                grid[i] = [];
+
+                for (var j = 0; j < this.config.grid.numberCellsY; j++) { // for each cell
+
+                    grid[i][j] = this.getRandomCell();
+
+                }
+
+            }
+
+            return grid;
+
+        },
+
+        getRandomCell: function(){
+            return !!Math.floor(Math.random() * 2);
+        },
+
+        drawGrid: function(grid) {
+
+            for (var i = 0; i < grid.length; i++) { // for each line
+
+                for (var j = 0; j < grid[i].length; j++) { // for each cell
+
+                    var cellInfo = grid[i][j];
+
+                    if (cellInfo) {
+
+                        var positionX = i * this.config.cell.width;
+                        var positionY = j * this.config.cell.height;
+
+                        this.drawNode('rect', {
+                                x: positionX,
+                                y: positionY,
+                                width: this.config.cell.width,
+                                height: this.config.cell.height,
+                                fill: '#000000'
+                            }
+                        );
+
+                    }
+
+                }
+
+            }
+
+        },
+
+        drawNode: function(nodeType, properties) {
+
+            var node = this.getNode(nodeType, properties);
+
+            this.canvas.appendChild(node);
+
+            return this;
+
+        },
+
+        getNode: function (nodeType, properties) {
+
+            var node = document.createElementNS("http://www.w3.org/2000/svg", nodeType);
+
+            for (var property in properties)
+                node.setAttributeNS(
+                    null,
+                    property.replace(/[A-Z]/g, function (match) { // transform camel case to dash case
+                        return "-" + match.toLowerCase();
+                    }),
+                    properties[property]
+                );
+            return node;
+        },
+
+        setupCanvas: function(config) {
+
+            var canvas = this.$(config.grid.selector);
+            canvas.width(config.grid.width);
+            canvas.height(config.grid.height);
+
+            this.config.cell = {
+                width: config.grid.width / config.grid.numberCellsX,
+                height: config.grid.height / config.grid.numberCellsY
+            };
+
+            return canvas.get(0);
 
         }
 
