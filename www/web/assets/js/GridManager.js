50dfa5c7a25ddca8550a26d5ed52bef6b6f3a0c6
Carlos Garcia-Berro Molero <gbmcarlos@gmail.com>
2017-11-06 15:25:26 +0400

    empty grid manager being included from the random pattern page

---
+++ www/web/assets/js/GridManager.js
@@ -0,0 +1,18 @@
+/**
+ * Created by gbmcarlos on 11/6/17.
+ */
+(function(jQuery, window){
+
+    var QuoteManager = {
+
+        init: function() {
+            console.log('TEST');
+        }
+
+    };
+
+    window.QuoteManager = QuoteManager;
+
+    window.QuoteManager.init();
+
+})(jQuery, window);
