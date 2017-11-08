475e8e859f930b4ecb4ddd11228233d3a7f8c9cb
Carlos Garcia-Berro Molero <gbmcarlos@gmail.com>
2017-11-08 11:13:54 +0400

    both / and /index.php redirect to the front page

--- www/app/config/Routing.php
+++ www/app/config/Routing.php
@@ -15,8 +15,10 @@ class Routing {
     public static function registerRoutes(Application $app){
 
         // Register routes here
-        $app->get('/index.php', "FrontController:index")
+        $app->get('/', "FrontController:index")
             ->bind('front.index');
+        $app->get('/index.php', "FrontController:index")
+            ->bind('front.index2');
         $app->get('/random-pattern', "FrontController:randomPattern")
             ->bind('front.random-pattern');
         $app->get('/gosper-glider-gun', "FrontController:gosperGliderGun")
